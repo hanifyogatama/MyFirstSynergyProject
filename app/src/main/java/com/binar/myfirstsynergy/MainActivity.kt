148523c8f5a4a798094a21c6a41a8cea289ee4d5
Hanif yogatama <hanifyotama@gmail.com>
2020-07-15 14:26:48 +0700

    kotlin synthetic done

---
+++ app/src/main/java/com/binar/myfirstsynergy/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.binar.myfirstsynergy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+// Kotlin Synthetic -------------------------------
+        btnKotlinSynthetic.setOnClickListener{
+            Log.d("Binar","Tombol login di klik")
+        }
+    }
+}
+
