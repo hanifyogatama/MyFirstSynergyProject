bbbabff6e0a77b5f519068a2d504474a22b456ca
Hanif yogatama <hanifyotama@gmail.com>
2020-07-15 15:32:35 +0700

    add findById()

--- app/src/main/java/com/binar/myfirstsynergy/MainActivity.kt
+++ app/src/main/java/com/binar/myfirstsynergy/MainActivity.kt
@@ -3,7 +3,10 @@ package com.binar.myfirstsynergy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,23 @@ class MainActivity : AppCompatActivity() {
 // Kotlin Synthetic -------------------------------
         btnKotlinSynthetic.setOnClickListener{
             Log.d("Binar","Tombol login di klik")
+        }
+
+
+        // find by id -------------------------------
+
+        // cara 1
+//      val btnFindIdMessage : Button = findViewById(R.id.btnFindById)
+//      btnFindIdMessage.text = "hi, i'm clicked"
+
+        // cara 2
+        var outputFindViewId: TextView? = null
+        var myButton: Button ?= null
+
+        outputFindViewId = findViewById(R.id.tvHelloSynergy)
+        myButton= findViewById(R.id.btnFindById)
+        myButton?.setOnClickListener {
+            outputFindViewId?.text = getString(R.string.click_me)
         }
     }
 }
